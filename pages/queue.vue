f5960ed6a36026606a29ff29bf626a24851e93c1
kvasir111 <rrowe222@gmail.com>
2020-01-30 15:04:02 -0600

    moved the async function to a mounted hook

--- pages/queue.vue
+++ pages/queue.vue
@@ -5,7 +5,7 @@
         <h1>In progress repairs</h1>
       </div>
       <div class="bg-white rounded-b">
-        <div id="repairList">
+        <div id="repairList" :key="index" v-for="(repair, index) in repairList">
 
         </div>
       </div>
@@ -22,50 +22,30 @@
 
         data : function(){
             return{
+                repairList : [],
             }
         },
-
-        async asyncData(){
+        async mounted() {
             //repairList is an array of repair objects that will be used to create the repair queue items
-          let repairList = [];
-          await firebase
-              .firestore()
-              .collection("Repair Queue")
-              .get()
-              //the snapshot is what the database looks like when the function is called
-              .then(snapshot =>{
-                  //a doc is essentially a record in firebase, each doc con contain literally anything, but in this case each one represents a repair item
-                  snapshot.forEach(doc =>{
-                      repairList.push({
-                          //the doc ID will probably be used like a Service Order Number until I can come up with a better solution
-                          repairID: doc.id,
-                          //the data is 3 objects, customerObject, deviceObject, and repairObject, each one of them has some strings underneath it
-                          repairData: doc.data()
-                      })
-                  })
-              })
-        },
-
-        created() {
-            //fetches all the in progress tickets when the page is created
-            //this.fetchRepairs()
-        },
-        methods: {
-            fetchRepairs(){
-                const database = firebase.firestore();
-
-                //gets the collection of in progress repairs
-                const repairsRef = database.collection('Repair Queue');
-
-                let allRepairs = repairsRef.get().then(snapshot =>{
-                    //the snapshot is basically just that, what the database looks like when the function is called
-                    snapshot.forEach(doc =>{
-                        console.log(doc.data());
-                        //pushes the list of docs into the "list"
-                        this.list.push(doc);
+            await firebase
+                .firestore()
+                .collection("Repair Queue")
+                .get()
+                //the snapshot is what the database looks like when the function is called
+                .then(snapshot => {
+                    //a doc is essentially a record in firebase, each doc con contain literally anything, but in this case each one represents a repair item
+                    snapshot.forEach(doc => {
+                        this.repairList.push({
+                            //the doc ID will probably be used like a Service Order Number until I can come up with a better solution
+                            repairID: doc.id,
+                            //the data is 3 objects, customerObject, deviceObject, and repairObject, each one of them has some strings underneath it
+                            repairData: doc.data()
+                        });
+                        console.log(doc.data())
                     })
                 })
-            },
+        },
+        methods: {
         }
     };
 </script>
