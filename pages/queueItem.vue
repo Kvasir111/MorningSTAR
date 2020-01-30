73b11b47c280e4f8cf45c5093c8d2246595124b6
kvasir111 <rrowe222@gmail.com>
2020-01-30 16:43:37 -0600

    Repair Queue now populates correctly with the data from the database

--- pages/queueItem.vue
+++ pages/queueItem.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="bg-white rounded w-1/3">
-    <ul>
-      <li>{{repair}}</li>
+  <div class="bg-white rounded w-1/3 m-2 ">
+    <ul id="customerInformation">
+      <li>{{clientName}}</li>
+      <li>{{accountNumber}}</li>
+      <li>{{address}}</li>
+    </ul>
+
+    <ul id="deviceInformation">
+      <li>{{deviceMake}}</li>
+      <li>{{deviceModel}}</li>
+    </ul>
+
+    <ul id="repairInformation">
+      <li>{{checkInDate}}</li>
+      <li>{{checkInReason}}</li>
     </ul>
   </div>
 </template>
@@ -9,14 +21,28 @@
 <script>
     export default {
         name: "queueItem",
+      //the data from the firebase query is passed as an object, then parsed individually in the component
         props: ["repair"],
         data: function () {
             return {
-                clientName: '',
+
+              //basic customer information, the first and last name come as separate data members but i've concatenated them here
+              clientName: this.repair.repairData.customerObject["First Name"] + ' ' + this.repair.repairData.customerObject["Last Name"],
+              accountNumber: this.repair.repairData.customerObject['Account #'],
+              address: this.repair.repairData.customerObject['Address'],
+
+              //device information, such as computer make and model
+              deviceMake: this.repair.repairData.deviceObject["Make"],
+              deviceModel: this.repair.repairData.deviceObject["model"],
+
+              //repair information, this is the description of the issue and when it was checked in, some of this might get moved to a different call since this can get lengthy if people are verbose
+              checkInDate: this.repair.repairData.repairObject["Check In Date"],
+              checkInReason: this.repair.repairData.repairObject["Check In Reason"]
             }
         },
         created() {
-          console.log("created new item")
+          //for debugging, just logs the event into the console
+          console.log("created new item");
         }
     }
 </script>
