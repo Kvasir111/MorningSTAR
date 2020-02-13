ccb8f34f687d0a590272f3d92be73063138d03fd
kvasir111 <rrowe222@gmail.com>
2020-02-13 16:35:02 -0600

    Applied new changes to the repair Queue

--- components/archive_item.vue
+++ components/archive_item.vue
@@ -21,8 +21,8 @@
 			</ul>
 		</div>
 
-		<div class="inline-block align-bottom justify-end">
-			<button v-on:click="showDetails" v-bind:id="SOdetailButton" class="bg-orange-600 px-2 inline fontMont text-white rounded-lg ">
+		<div class="inline-block ">
+			<button v-on:click="showDetails" v-bind:id="SOdetailButton" class="bg-orange-600 px-2 inline fontMont align-bottom text-white rounded-lg ">
 				{{text}}
 					<svg  class="inline" xmlns="http://www.w3.org/2000/svg"
 						  width="30" height="30" viewBox="0 0 24 24" fill="none"
---
+++ components/close_repairButton.vue
@@ -0,0 +1,17 @@
+<template>
+	<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" v-bind:stroke="strokeColor" v-bind:stroke-width="width" stroke-linecap="round" stroke-linejoin="round">
+		{{text}}
+		<path d="M20 6L9 17l-5-5"/>
+	</svg>
+</template>
+
+<script>
+    export default {
+		props: ['strokeColor', 'width', 'text'],
+        name: 'close_repairButton'
+    };
+</script>
+
+<style scoped>
+
+</style>
