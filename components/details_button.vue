1fa62c3ba4ee162061a7082280f21721fde4d48d
kvasir111 <rrowe222@gmail.com>
2020-02-13 15:44:05 -0600

    Reactored the details button for the archive, needs to be applied to the rest of the card objects

---
+++ components/details_button.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="bg-orange-800 px-2 text-white fontMont font-bold" v-on:click="rotate">
+		{{text}}
+		<svg  class="inline" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" v-bind:stroke="strokeColor" v-bind:stroke-width="width" stroke-linecap="round" stroke-linejoin="round">
+			<path d="M6 9l6 6 6-6"/>
+		</svg>
+	</div>
+</template>
+
+<script>
+    export default {
+    	props: ['strokeColor', 'width', 'bgColor', 'text'],
+        name: 'details_button',
+		methods: {
+    		rotate:{
+
+			}
+		}
+    };
+</script>
+
+<style scoped>
+
+</style>
--- pages/queue.vue
+++ pages/queue.vue
@@ -43,6 +43,7 @@
                 })
         },
         methods: {
+
         }
     };
 </script>
