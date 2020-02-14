25691557c9b05604b59fb719893242c1c3b2364d
kvasir111 <rrowe222@gmail.com>
2020-02-14 11:44:21 -0600

    Added some buttons to the homepage so you dont' have to open a nav menu to go anywhere

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,42 @@
 <template>
-  <div class="h-screen flex">
-  <div class="mt-4 md:m-auto w-full">
-	  <img v-bind:src="logo" alt="morningSTAR logo">
-  </div>
-  </div>
+	<div class="h-screen flex">
+		<div class="mt-4 md:m-auto w-full">
+			<img v-bind:src="logo" alt="morningSTAR logo">
+			<div class="text-center w-full block md:flex justify-center">
+				<nuxt-link :key="index" class="homeLink md:w-1/6 mx-auto block hover:bg-black hover:text-orange-500"
+						   v-for="(link, index) in links" v-bind:to="link.link">{{link.linkText}}
+				</nuxt-link>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+	import Logo from '~/components/Logo.vue';
 
-export default {
-  components: {
-    Logo
-  },
-  data: function() {
-	return{
-		logo: require('../assets/morningStar.svg'),
-	}
-  }
-}
+	export default {
+		components: {
+			Logo
+		},
+		data: function() {
+			return {
+				logo: require('../assets/morningStar.svg'),
+
+				links: [
+					{ linkText: 'New Repair', link: '/new_repair' },
+					{ linkText: 'QuickFix', link: '/quickfix' },
+					{linkText: 'Repair Queue', link: '/queue'},
+					{linkText: 'Checkout', link: '/checkout'}
+				]
+			};
+		}
+	};
 </script>
 
 <style>
-  .homeLink{
-    @apply block text-center rounded p-2
-  }
+	.homeLink {
+		@apply bg-white px-4 m-2 py-2 text-xl rounded-lg;
+		font-family: 'Montserrat', sans-serif;
+	}
 
 </style>
